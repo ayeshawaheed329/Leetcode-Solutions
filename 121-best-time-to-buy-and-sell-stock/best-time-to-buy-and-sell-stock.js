cc4773f2af6f16fc7085f10354af70844df2e004
Ayesha Waheed <48578743+ayeshawaheed329@users.noreply.github.com>
2025-07-14 11:47:41 +0500

    Time: 7 ms (11.03%) | Memory: 65.2 MB (50.54%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    
+    let min = Infinity;
+    let maxProfit = 0;
+    for(let price of prices){
+        if(price < min){
+            min = price;
+        }else {
+            maxProfit = Math.max(maxProfit, price - min); 
+        }
+    }
+    return maxProfit;
+};
